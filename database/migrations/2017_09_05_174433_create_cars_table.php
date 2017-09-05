2e34f527f23784f0d6fd0b5bb0fbe2b3eb1f2095
Mladen <djoml8bre@gmail.com>
2017-09-05 19:52:50 +0200

    First commit

---
+++ database/migrations/2017_09_05_174433_create_cars_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCarsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cars', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('mark');
+            $table->string('model');
+            $table->integer('year');
+            $table->integer('maxSpeed');
+            $table->boolean('isAutomatic');
+            $table->string('engine');
+            $table->integer('numberOfDoors');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cars');
+    }
+}
